8079b269ab7761656c9e2e7f9eb9d4f962a4267f
vic-ferr <victorhfp2014@gmail.com>
2022-11-11 20:35:34 -0300

    conexion a la base de datos

--- src/bsale.js
+++ src/bsale.js
@@ -1,22 +1,26 @@
-// const express = require("express");
-// const app = express();
+const express = require("express");
+const app = express();
+const cors = require("cors")
 // const morgan = require("morgan");
 const mysql = require("mysql");
 require("dotenv").config()
 
-//settings
-// app.set("port", process.env.PORT || 3000)
+const PORT = process.env.PORT || 3600
+
+app.use(express.json())
+app.use(cors())
 
 
 
 // app.use(morgan("dev"));
-const conection = mysql.createPool({
+mysql.createPool({
     host: process.env.HOST,
     usuario: process.env.USER,
     contarseña: process.env.PASSWORD,
     database: process.env.DB
 })
 
+app.listen(PORT, () => console.log(`hay conexion en puerto${PORT}`));
 
 // app.listen(app.get("port"), () => {
 //     console.log("hay conexion");
